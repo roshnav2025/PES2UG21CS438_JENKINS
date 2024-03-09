292c49e02b8fe788cf3d85bd9af1b0fe3eff530e
roshnav2025 <147706862+roshnav2025@users.noreply.github.com>
2024-03-09 19:16:46 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "webhook successfully enabled\n";
+  return 0;
+}
